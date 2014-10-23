a4c1470492b30549e36f17321239ae9f65dd33b1
dstrockis <dastrock@microsoft.com>
2014-10-23 13:01:52 -0700

    jq picker checkpoint

--- WebApp-GroupClaims-DotNet/App_Start/BundleConfig.cs
+++ WebApp-GroupClaims-DotNet/App_Start/BundleConfig.cs
@@ -8,7 +8,8 @@ namespace WebAppGroupClaimsDotNet
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery-ui.min.js"
                 ));
 
             bundles.Add(new ScriptBundle("~/Scripts/AadPickerLibrary").Include(
--- WebApp-GroupClaims-DotNet/Scripts/AadPickerLibrary.js
+++ WebApp-GroupClaims-DotNet/Scripts/AadPickerLibrary.js
@@ -15,7 +15,7 @@
         this.userSkipToken = null;
         this.groupSkipToken = null;
         this.lastDisplayed = "";
-        this.lastInput;
+        //this.lastInput;
         this.isPaging = false;
         this.selectColor;
         this.unSelectColor;
@@ -25,8 +25,12 @@
         this.graphLoc = "https://graph.windows.net";
         this.apiVersion = "1.22-preview";
 
+        // UI Labels
+        this.userLabel = "";
+        this.groupLabel = "(group)";
+
         // Activate
-        this.SearchBoth("");
+        //this.SearchBoth("");
         this.Listen();
 
         // Get CSS Properties for Dynamic Color Changes
@@ -45,20 +49,25 @@
     }
 
     AadPicker.prototype.Listen = function () {
-
-        var timer = null;
-        this.$input.on('input', { picker: this }, function (event) {
-            var val = $(this).val();
-            function runSearch(event, val) {
-                event.data.picker.SearchBoth(val);
-            }
-            if (timer) {
-                window.clearTimeout(timer);
-            }
-            timer = window.setTimeout(function () {
-                runSearch(event, val);
-            }, 200);
-        });
+        var picker = this;
+        this.$input.catcomplete({
+            source: function (request, response) {
+                picker.Search(request.term, response);
+            },
+            minLength: 0,
+            delay: 500,
+            open: function (event, ui) {
+                //picker.Select();
+            },
+            select: function (event, ui) {
+                event.preventDefault();
+            },
+            focus: function (event, ui) {
+                event.preventDefault();
+            },
+        }).data("custom-catcomplete").close = function (event) {
+            return false;
+        }
     }
 
     AadPicker.prototype.ConstructUserQuery = function (inputValue) {
@@ -119,12 +128,7 @@
         });
     }
 
-    AadPicker.prototype.SearchBoth = function (inputValue) {
-
-        this.lastInput = inputValue;
-
-        if (inputValue == undefined)
-            inputValue = "";
+    AadPicker.prototype.Search = function (inputValue, callback) {
 
         var userQuery = this.ConstructUserQuery(inputValue);
         var groupQuery = this.ConstructGroupQuery(inputValue);
@@ -132,14 +136,15 @@
         var userDeffered = this.SendQuery(userQuery);
         var groupDeffered = this.SendQuery(groupQuery);
 
-        var recordResults = function (picker, inputValue) {
+        var recordResults = function (picker, inputValue, callback) {
             return function (userQ, groupQ) {
-                var results = { error: "Error during searching" };
+                var results;
 
-                if (userQ[1] == "success" && groupQ[1] == "success") {
+                if (userQ[1] == "success" && groupQ[1] == "success" 
+                    && userQ[0].error == undefined && groupQ[0].error == undefined) {
 
-                    if (userQ[0].error == undefined && groupQ[0].error == undefined)
-                        results = groupQ[0].value.concat(userQ[0].value);
+                    results = [];
+                    var usersAndGroups = userQ[0].value.concat(groupQ[0].value);
 
                     if (userQ[0]["odata.nextLink"] != undefined) {
                         picker.userSkipToken = userQ[0]["odata.nextLink"]
@@ -160,239 +165,68 @@
 
                     if (inputValue != picker.lastDisplayed)
                         picker.currentResults = {};
-                    if (results.error == undefined) {
-                        for (var i = 0; i < results.length; i++) {
-                            picker.currentResults[results[i].objectId] = {
-                                objectId: results[i].objectId,
-                                displayName: results[i].displayName,
-                                objectType: results[i].objectType
-                            };
-                        }
-                    }
 
+                    for (var i = 0; i < usersAndGroups.length; i++) {
+
+                        if (usersAndGroups[i].objectType == "User") {
+                            results.push({
+                                label: usersAndGroups[i].displayName,
+                                value: usersAndGroups[i].displayName,
+                                objectId: usersAndGroups[i].objectId,
+                                objectType: picker.userLabel,
+                            });
+                        } else if (usersAndGroups[i].objectType == "Group") {
+                            results.push({
+                                label: usersAndGroups[i].displayName,
+                                value: usersAndGroups[i].displayName,
+                                objectId: usersAndGroups[i].objectId,
+                                objectType: picker.groupLabel,
+                            });
+                        }
+                        
+
+                        picker.currentResults[usersAndGroups[i].objectId] = {
+                            objectId: usersAndGroups[i].objectId,
+                            displayName: usersAndGroups[i].displayName,
+                            objectType: usersAndGroups[i].objectType
+                        };
+                    }
                 }
                 else {
                     picker.currentResults = {};
+                    results = [{ label: "Error During Search" }];
                 }
-
-                if (picker.lastInput == inputValue)
-                    picker.Display(results, inputValue);
-
+                
+                picker.selected = null; //TODO
+                callback(results);
             };
         };
 
         $.when(userDeffered, groupDeffered)
-            .always(recordResults(this, inputValue))
+            .always(recordResults(this, inputValue, callback))
     };
 
-    AadPicker.prototype.SearchUserOnly = function (inputValue) {
+    $.widget("custom.catcomplete", $.ui.autocomplete, {
+        _create: function () {
+            this._super();
+            this.widget().menu("option", "items", "> :not(.ui-autocomplete-category)");
+        },
+        _renderMenu: function (ul, items) {
+            var that = this;
 
-        this.lastInput = inputValue;
+            $.each(items, function (index, item) {
+                that._renderItemData(ul, item);
+            });
+        },
+        _renderItem: function (ul, item) {
+            var label = $("<div>").addClass("aadpicker-result-label").append(item.label);
+            var type = $("<div>").addClass("aadpicker-result-type").append(item.objectType);
+            var toappend = [label, type];
 
-        var userQuery = this.ConstructUserQuery(inputValue);
-        var userDeffered = this.SendQuery(userQuery);
-
-        var recordResults = function (picker, inputValue) {
-            return function (data, textStatus, jqxhr) {
-                var results = { error: "Error during searching" };
-
-                if (textStatus == "success") {
-
-                    if (data.error == undefined)
-                        results = data.value;
-
-                    if (data["odata.nextLink"] != undefined) {
-                        picker.userSkipToken = data["odata.nextLink"]
-                            .substring(data["odata.nextLink"].indexOf("$skiptoken"),
-                            data["odata.nextLink"].length);
-                    }
-                    else {
-                        picker.userSkipToken = null;
-                    }
-
-                    if (inputValue != picker.lastDisplayed)
-                        picker.currentResults = {};
-                    if (results.error == undefined) {
-                        for (var i = 0; i < results.length; i++) {
-                            picker.currentResults[results[i].objectId] = {
-                                objectId: results[i].objectId,
-                                displayName: results[i].displayName,
-                                objectType: results[i].objectType
-                            };
-                        }
-                    }
-                }
-                else {
-                    picker.currentResults = {};
-                }
-                if (picker.lastInput == inputValue)
-                    picker.Display(results, inputValue);
-            };
-
-        };
-
-        userDeffered.always(recordResults(this, inputValue))
-    };
-
-    AadPicker.prototype.SearchGroupOnly = function (inputValue) {
-
-        this.lastInput = inputValue;
-
-        var groupQuery = this.ConstructGroupQuery(inputValue);
-        var groupDeffered = this.SendQuery(groupQuery);
-
-        var recordResults = function (picker, inputValue) {
-            return function (data, textStatus, jqxhr) {
-                var results = { error: "Error during searching" }; 
-
-                if (textStatus == "success") {
-
-                    if (data.error == undefined)
-                        results = data.value;
-
-                    if (data["odata.nextLink"] != undefined) {
-                        picker.groupSkipToken = data["odata.nextLink"]
-                            .substring(data["odata.nextLink"].indexOf("$skiptoken"),
-                            data["odata.nextLink"].length);
-                    }
-                    else {
-                        picker.groupSkipToken = null;
-                    }
-
-                    if (inputValue != picker.lastDisplayed)
-                        picker.currentResults = {};
-                    if (results.error == undefined) {
-                        for (var i = 0; i < results.length; i++) {
-                            picker.currentResults[results[i].objectId] = {
-                                objectId: results[i].objectId,
-                                displayName: results[i].displayName,
-                                objectType: results[i].objectType
-                            };
-                        }
-                    }
-                }
-                else {
-                    picker.currentResults = {};
-                }
-                
-                if (picker.lastInput == inputValue)
-                    picker.Display(results, inputValue);
-            };
-
-        };
-
-        groupDeffered.always(recordResults(this, inputValue));
-    };
-
-    AadPicker.prototype.SelectHandler = function () {
-        if ($(this).attr("data-selected") == "true") {
-            $(this).attr("data-selected", "false");
-            $(this).css("background-color", picker.unSelectColor);
-            picker.selected = null;
-            picker.$input.get()[0].value = picker.lastDisplayed;
+            return $("<li>").addClass("aadpicker-result-elem").attr("data-selected", "false")
+                .attr("data-objectId", item.objectId).append(toappend).appendTo(ul);
         }
-        else if ($(this).attr("data-selected") == "false") {
-            $(".aadpicker-result-elem").attr("data-selected", "false").css("background-color", picker.unSelectColor);
-            $(this).attr("data-selected", "true");
-            $(this).css("background-color", picker.selectColor);
-            picker.selected = picker.currentResults[$(this).attr("data-objectId")];
-            picker.$input.get()[0].value = $(this).text();
-        }
-    };
-
-    AadPicker.prototype.MouseEnterHandler = function () {
-        if ($(this).attr("data-selected") == "false")
-            $(this).css("background-color", picker.hoverColor);
-    };
-
-    AadPicker.prototype.MouseLeaveHandler = function () {
-        if ($(this).attr("data-selected") == "false")
-            $(this).css("background-color", picker.unSelectColor);
-    };
-
-    AadPicker.prototype.ScrollHandler = function () {
-
-        if ($(this).scrollTop() + $(this).innerHeight() >= this.scrollHeight && !picker.isPaging)
-            picker.Page();
-    };
-
-    AadPicker.prototype.Page = function () {
-        if (this.userSkipToken && this.groupSkipToken) {
-            this.SearchBoth(this.lastDisplayed);
-            this.isPaging = true;
-        }
-        else if (this.userSkipToken) {
-            this.SearchUserOnly(this.lastDisplayed);
-            this.isPaging = true;
-        }
-        else if (this.groupSkipToken) {
-            this.SearchGroupOnly(this.lastDisplayed);
-            this.isPaging = true;
-        }
-    };
-
-    AadPicker.prototype.Select = function ($resultsDiv) {
-
-        $resultsDiv.off("click", ".aadpicker-result-elem", this.SelectHandler)
-            .on("click", ".aadpicker-result-elem", { picker: this }, this.SelectHandler);
-
-        $resultsDiv.off("mouseenter", ".aadpicker-result-elem", this.MouseEnterHandler)
-            .off("mouseleave", ".aadpicker-result-elem", this.MouseLeaveHandler)
-            .on("mouseenter", ".aadpicker-result-elem", { picker: this }, this.MouseEnterHandler)
-            .on("mouseleave", ".aadpicker-result-elem", { picker: this }, this.MouseLeaveHandler);
-    };
-
-    AadPicker.prototype.Paging = function ($resultsDiv) {
-
-        this.isPaging = false;
-
-        $resultsDiv.unbind("scroll", this.ScrollHandler)
-            .bind("scroll", { picker: this }, this.ScrollHandler);
-
-        if ($resultsDiv.get(0).offsetHeight >= $resultsDiv.get(0).scrollHeight)
-            this.Page();
-    };
-
-    AadPicker.prototype.Display = function (results, inputValue) {
-        
-        var $resultsDiv = $(".aadpicker-search-results");
-        if ($resultsDiv.length == 0) {
-            $resultsDiv = $("<div></div>").addClass("aadpicker-search-results");
-            this.$input.after($resultsDiv);
-        }
-
-        if (results.error != undefined) {
-            var $msg = $("<p>" + results.error + "</p>").css("color", "red");
-            this.lastDisplayed = results.error;
-            $resultsDiv.empty();
-            $resultsDiv.append($msg);
-            return;
-        }
-
-        var toAppend = [];
-        for (var i = 0; i < results.length; i++) {
-            var $elem;
-            if (results[i].objectType == "Group") {
-                $elem = $("<div>" + results[i].displayName + " (Security Group)</div>").addClass("aadpicker-result-elem").attr("data-selected", "false").attr("data-objectid", results[i].objectId);
-            }
-            else if (results[i].objectType == "User") {
-                $elem = $("<div>" + results[i].displayName + " (" + results[i].userPrincipalName + ")</div>").addClass("aadpicker-result-elem").attr("data-selected", "false").attr("data-objectid", results[i].objectId);
-            }
-
-            toAppend.push($elem);
-        }
-
-        if (inputValue != this.lastDisplayed || this.lastDisplayed == "Error during searching" || !this.isPaging)
-            $resultsDiv.empty();
-        $resultsDiv.append(toAppend);
-
-        this.lastDisplayed = inputValue;
-
-        this.Select($resultsDiv);
-
-        this.Paging($resultsDiv);
-    }
-
+    });
 
     return new AadPicker();
 };
